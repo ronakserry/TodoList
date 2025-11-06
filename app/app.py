bbe2c51f06afc4982b9de0c230490b472e26f44a
rounak serri <rounalserry@gmail.com>
2025-11-06 12:20:15 +0330

    added screenshot

--- app/app.py
+++ app/app.py
@@ -1,20 +1,30 @@
 from flask import Flask, request, make_response, render_template
 
-app = Flask(__name__)
-@app.route("/")
-def index():
-    return render_template('index.html')
 
-@app.route("/search")
-def search():
-    return render_template('search.html')
+def create_app() -> Flask:
+    app = Flask(__name__)
 
-@app.route("/calendar")
-def calendar():
-    return render_template('calendar.html')
 
-@app.route("/settings")
-def settings():
-    return render_template('settings.html')
+    @app.route("/")
+    def index():
+        return render_template('index.html')
+
+    # The following views assume corresponding templates exist.
+    @app.route("/search")
+    def search():
+        return render_template('search.html')
+
+    @app.route("/calendar")
+    def calendar():
+        return render_template('calendar.html')
+
+    @app.route("/settings")
+    def settings():
+        return render_template('settings.html')
+
+    return app
+
+
 if __name__ == '__main__':
+    app = create_app()
     app.run(host='0.0.0.0', port=5555, debug=True)
